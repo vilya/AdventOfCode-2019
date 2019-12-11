1ce60ba70080d4b70900ff4f95d31100e22bd25c
Vilya Harvey <vilya.harvey@gmail.com>
2019-12-11 22:40:55 +0000

    Remove conditional compilation from day 7 solutions
    The conditional compilation was so that I could easily switch between the tiny intcode vm and the old ("large"?) vm, to do performance comparisons. I've decided to go all in on the tiny vm now, so this commit removes the code that used the large vm.

--- day07/day07a.cpp
+++ day07/day07a.cpp
@@ -1,11 +1,6 @@
 #define TINY_INTCODE 1
 
-#if TINY_INTCODE
 #include <tiny-intcode.h>
-#else
-#include <intcode.h>
-#include <queue>
-#endif
 
 #include <algorithm>
 #include <cstdio>
@@ -29,37 +24,16 @@ int main(int argc, char** argv)
   std::vector<int> perm{ 0, 1, 2, 3, 4 };
   int64_t maxOutput = 0;
   do {
-//    printf("perm = { %d, %d, %d, %d, %d }\n", perm[0], perm[1], perm[2], perm[3], perm[4]);
-
-#if TINY_INTCODE
     int64_t output = 0;
     for (int phase : perm) {
-//      printf("  phase = %d, input = %lld --> ", phase, output);
       vh::IntcodeComputer prog = src;
       prog.run(phase);
       prog.run(output);
       output = prog.out[0];
-//      printf("output = %lld\n", output);
     }
     if (output > maxOutput) {
       maxOutput = output;
     }
-#else
-    std::vector<int64_t> output{ 0 };
-    for (int phase : perm) {
-//      printf("  phase = %d, input = %lld --> ", phase, output.back());
-      vh::IntcodeComputer prog = src;
-      std::queue<int64_t> input;
-      input.push(phase);
-      input.push(output.back());
-      output.clear();
-      prog.run(input, output);
-//      printf("output = %lld\n", output.back());
-    }
-    if (output.back() > maxOutput) {
-      maxOutput = output.back();
-    }
-#endif
   } while (std::next_permutation(perm.begin(), perm.end()));
 
   printf("Max output = %lld\n", maxOutput);
--- day07/day07b.cpp
+++ day07/day07b.cpp
@@ -1,11 +1,4 @@
-#define TINY_INTCODE 1
-
-#if TINY_INTCODE
 #include <tiny-intcode.h>
-#else
-#include <intcode.h>
-#include <queue>
-#endif
 
 #include <algorithm>
 #include <cstdio>
@@ -29,42 +22,19 @@ int main(int argc, char** argv)
   std::vector<int> perm{ 5, 6, 7, 8, 9 };
   int maxOutput = 0;
   do {
-//    printf("perm = { %d, %d, %d, %d, %d }\n", perm[0], perm[1], perm[2], perm[3], perm[4]);
-
     vh::IntcodeComputer prog[5] = { src, src, src, src, src };
 
-#if TINY_INTCODE
-    int64_t status, amount = 0;
     for (size_t i = 0; i < 5; i++) {
       prog[i].run(perm[i]);
     }
+
+    int64_t status, amount = 0;
     do {
       for (size_t i = 0; i < 5; i++) {
         status = prog[i].run(amount);
         amount = prog[i].out[0];
       }
     } while (status != 0);
-#else
-    vh::Status status;
-    std::vector<int64_t> output;
-    int amount = 0;
-    bool hasPhase = false;
-    do {
-      for (size_t i = 0; i < 5; i++) {
-        int phase = perm[i];
-//        printf("  phase = %d, input = %d --> ", phase, amount);
-        std::queue<int64_t> input;
-        if (!hasPhase) {
-          input.push(phase);
-        }
-        input.push(amount);
-        status = prog[i].run(input, output);
-        amount = output.back();
-//        printf("output = %d\n", amount);
-      }
-      hasPhase = true;
-    } while (status != vh::Status::Halted);
-#endif
 
     if (amount > maxOutput) {
       maxOutput = amount;
