484075c326d7bfd739e6817195ce8e4570af322d
Vilya Harvey <vilya.harvey@gmail.com>
2019-12-17 01:00:46 +0000

    Use uint8_t for storage and int for the running sum in day16b
    Using uint8_t for storage instead of int64_t reduces memory traffic by a factor of 8.
    
    Using an int instead of an int64_t for the running sum inside the apply_pattern function, since testing shows that arithmetic is a bit faster with an int.
    
    Together these reduce the total runtime by about 15 ms, from ~80 ms to ~65 ms.

--- day16/day16b.cpp
+++ day16/day16b.cpp
@@ -6,14 +6,14 @@
 constexpr size_t kRepeat = 10000;
 
 
-bool load(const char* filename, std::vector<int64_t>& out)
+bool load(const char* filename, std::vector<uint8_t>& out)
 {
   FILE* f = fopen(filename, "r");
   if (!f) {
     return false;
   }
 
-  int64_t tmp;
+  int tmp;
   size_t offset = 0;
   for (int i = 0; i < 7; i++) {
     tmp = fgetc(f);
@@ -35,7 +35,7 @@ bool load(const char* filename, std::vector<int64_t>& out)
 
   out.resize(n * repeats);
   for (size_t i = 1; i < repeats; i++) {
-    std::memcpy(out.data() + i * n, out.data(), n * sizeof(int64_t));
+    std::memcpy(out.data() + i * n, out.data(), n * sizeof(uint8_t));
   }
   out.erase(out.begin(), out.begin() + offset % n);
 
@@ -43,12 +43,12 @@ bool load(const char* filename, std::vector<int64_t>& out)
 }
 
 
-void apply_pattern(const std::vector<int64_t>& in, std::vector<int64_t>& out)
+void apply_pattern(const std::vector<uint8_t>& in, std::vector<uint8_t>& out)
 {
   const size_t n = in.size();
   assert(out.size() == n);
-  int64_t sum = 0;
-  for (int64_t i = n - 1; i >= 0; --i) {
+  int sum = 0;
+  for (int i = n - 1; i >= 0; --i) {
     sum += in[i];
     out[i] = sum % 10;
   }
@@ -57,32 +57,23 @@ void apply_pattern(const std::vector<int64_t>& in, std::vector<int64_t>& out)
 
 int main(int argc, char** argv)
 {
-  std::vector<int64_t> data[2];
+  std::vector<uint8_t> data[2];
   if (!load(argv[1], data[0])) {
     fprintf(stderr, "Failed to load %s\n", argv[1]);
     return 1;
   }
   data[1].resize(data[0].size(), 0);
 
-  size_t offset = 0;
-  for (size_t i = 0; i < 7; i++) {
-    offset = offset * 10 + data[0][i];
-  }
-  if (offset * 2 < data[0].size()) {
-    fprintf(stderr, "Uh oh, can't solve this one: offset is in the first half of the data.\n");
-    return 1;
-  }
-
   const size_t numPhases = 100;
   for (size_t i = 0; i < numPhases; i++) {
     size_t src = i & 1;
     size_t dst = src ^ 1;
-    apply_pattern(data[src], data[dst], offset);
+    apply_pattern(data[src], data[dst]);
   }
 
   size_t finalDst = ((numPhases - 1) ^ 1) & 1;
   for (size_t i = 0; i < 8; i++) {
-    fputc(data[finalDst][i + offset] + '0', stdout);
+    fputc(data[finalDst][i] + '0', stdout);
   }
   printf("\n");
 
