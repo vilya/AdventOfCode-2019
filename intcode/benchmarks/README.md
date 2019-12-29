5016d37074b378bc4357c5f14501dd5d60e0db78
Vilya Harvey <vilya.harvey@gmail.com>
2019-12-29 23:22:33 +0000

    Support dynamically sized memory in the intcode vm.
    This makes it fractionally slower but means the VM is 100% correct according to the spec from Advent of Code, rather than "close enough for the AoC puzzles". In particular, it will no longer segfault if you try to run the sum-of-primes benchmark with a large enough input number.

--- intcode/benchmarks/README.md
+++ intcode/benchmarks/README.md
@@ -9,15 +9,18 @@ https://www.reddit.com/r/adventofcode/comments/egq9xn/2019_day_9_intcode_benchma
 Benchmark Results
 -----------------
 
-| Program       | Input      | Output                       | Runtime       |
-| ------------- | ---------- | ---------------------------- | ------------- |
-| sum-of-primes | 100000     | 454396537                    |     14.6 ms   |
-| ackermann     | 3,6        | 509                          |      8.3 ms   |
-| ackermann     | 4,1        | 65533                        | 129847.2 ms   |
-| isqrt         | 130        | 11                           |      0.012 ms |
-| divmod        | 1024,3     | 341,1                        |      0.012 ms |
-| factor        | 19338240   | 2,2,2,2,2,2,2,2,2,2,3,5,1259 |      1.8 ms   |
-| factor        | 2147483647 | 2147483647                   |     35.3 ms   |
+| Program       | Input         | Output                       | Runtime       |
+| ------------- | ------------- | ---------------------------- | ------------- |
+| sum-of-primes | 100000        | 454396537                    |     14.9 ms   |
+| sum-of-primes | 2000000       | 142913828922                 |    351.4 ms   |
+| sum-of-primes | 100000000     | 279209790387276              |  21326.7 ms   |
+| ackermann     | 3,6           | 509                          |      8.4 ms   |
+| ackermann     | 4,1           | 65533                        | 131578.4 ms   |
+| isqrt         | 130           | 11                           |      0.012 ms |
+| divmod        | 1024,3        | 341,1                        |      0.013 ms |
+| factor        | 19338240      | 2,2,2,2,2,2,2,2,2,2,3,5,1259 |      1.9 ms   |
+| factor        | 2147483647    | 2147483647                   |     37.5 ms   |
+| factor        | 1689259081189 | 1299709,1299721              |    822.9 ms   |
 
 Taken on a 2015 MacBook Pro with a 2.5 GHz Intel Core i7.
 
--- intcode/tiny-intcode.h
+++ intcode/tiny-intcode.h
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <queue>
 #include <vector>
 
@@ -13,42 +14,56 @@ namespace vh {
   struct IntcodeComputer {
     std::vector<int64_t> mem;
     std::vector<int64_t> out;
-    int64_t ip = 0, rel = 0;
+    int64_t ip = 0, rel = 0, size = 0;
 
     bool load(const char* filename) {
       FILE* f = fopen(filename, "r");
       if (f == nullptr)
         return false;
+      mem.reserve(INTCODE_MEM_SIZE);
       int64_t tmp;
       while (fscanf(f, "%lld,", &tmp) == 1)
         mem.push_back(tmp);
       fclose(f);
       if (mem.size() < INTCODE_MEM_SIZE)
         mem.resize(INTCODE_MEM_SIZE, 0ll);
+      size = static_cast<int64_t>(mem.size());
       out.reserve(16);
       return true;
     }
 
-    int64_t& loc(int64_t i) {
+    int64_t peek(int64_t i) const {
       int64_t mode = (mem[ip] / kModeDiv[i]) % 10;
-      return mem[((mode == 1) ? (ip + i) : mem[ip + i]) + ((mode == 2) ? rel : 0)]; 
+      int64_t addr = ((mode == 1) ? (ip + i) : mem[ip + i]) + ((mode == 2) ? rel : 0);
+      return (addr >= size) ? 0 : mem[addr];
+    }
+
+    int64_t& poke(int64_t i) {
+      int64_t mode = (mem[ip] / kModeDiv[i]) % 10;
+//      int64_t addr = ((mode == 1) ? (ip + i) : mem[ip + i]) + ((mode == 2) ? rel : 0);
+      int64_t addr = mem[ip + i] + ((mode == 2) ? rel : 0);
+      if (addr >= size) {
+        size = addr + INTCODE_MEM_SIZE;
+        mem.resize(static_cast<size_t>(size), 0ll);
+      }
+      return mem[addr];
     }
 
     // return value: 0 = halted, 1 = need input
     int run(std::deque<int64_t>& input) {
       out.clear();
       while (1) {
-        int64_t op = mem[ip];
-        switch (op % 100) {
-          case 1: loc(3) = loc(1) + loc(2); ip += 4; break; // add
-          case 2: loc(3) = loc(1) * loc(2); ip += 4; break; // mul
-          case 3: if (input.empty()) return 1; loc(1) = input.front(); input.pop_front(); ip += 2; break; // read
-          case 4: out.push_back(loc(1)); ip += 2; break; // write
-          case 5: ip = (loc(1) != 0) ? loc(2) : (ip + 3); break; // jump if true
-          case 6: ip = (loc(1) == 0) ? loc(2) : (ip + 3); break; // jump if false
-          case 7: loc(3) = (loc(1) <  loc(2)) ? 1 : 0; ip += 4; break; // less than
-          case 8: loc(3) = (loc(1) == loc(2)) ? 1 : 0; ip += 4; break; // equals
-          case 9: rel += loc(1); ip += 2; break; // set relative base
+        int64_t op = mem[ip] % 100;
+        switch (op) {
+          case 1: poke(3) = peek(1) + peek(2); ip += 4; break; // add
+          case 2: poke(3) = peek(1) * peek(2); ip += 4; break; // mul
+          case 3: if (input.empty()) return 1; poke(1) = input.front(); input.pop_front(); ip += 2; break; // read
+          case 4: out.push_back(peek(1)); ip += 2; break; // write
+          case 5: ip = (peek(1) != 0) ? peek(2) : (ip + 3); break; // jump if true
+          case 6: ip = (peek(1) == 0) ? peek(2) : (ip + 3); break; // jump if false
+          case 7: poke(3) = (peek(1) <  peek(2)) ? 1 : 0; ip += 4; break; // less than
+          case 8: poke(3) = (peek(1) == peek(2)) ? 1 : 0; ip += 4; break; // equals
+          case 9: rel += peek(1); ip += 2; break; // set relative base
           default: return 0;
         }
       }
