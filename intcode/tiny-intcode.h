cf26d02824f81bc7ecf319662e02b844133717a1
Vilya Harvey <vilya.harvey@gmail.com>
2020-01-02 16:03:19 +0000

    Improve speed of the intcode vm
    These changes give a 1.51x - 1.52x speedup in all of the test cases.
    
    Instead of calculating the addressing mode for each argument separately we now do the lookup for all args at once. We have a switch statement which returns a pointer to one of a small number of precalculated mode arrays. This is based on the observation that there are only 18 valid combinations of addressing modes for all three arguments (bearing in mind that mode 1 is not valid for the destination argument, so can never be used for argument 3). This we do a single div and mod per instruction now, instead of up to three of each.
    
    Replaced the address resolution logic in peek(), which consisted of two ternary if expressions, with a switch. This gives a surprisingly big speedup over the previous version, due (I think) to replacing the evaluation of two boolean expressions with a single computed jump.
    
    Casting the opcode to an int before doing the div and mod gives a small performance increase. I tried using std::div to get the remainder and quotient simultaneously, but this turned out to be a lot slower for some reason.
    
    Oddly the profile build ("release with debug info" in cmake) is faster than a release build!

--- intcode/tiny-intcode.h
+++ intcode/tiny-intcode.h
@@ -9,12 +9,32 @@
 
 namespace vh {
   
-  static constexpr int64_t kModeDiv[] = { 10, 100, 1000, 10000 };
+  static constexpr uint8_t kModes_000[4] = { 0, 0, 0, 0 };
+  static constexpr uint8_t kModes_001[4] = { 0, 1, 0, 0 };
+  static constexpr uint8_t kModes_002[4] = { 0, 2, 0, 0 };
+  static constexpr uint8_t kModes_010[4] = { 0, 0, 1, 0 };
+  static constexpr uint8_t kModes_011[4] = { 0, 1, 1, 0 };
+  static constexpr uint8_t kModes_012[4] = { 0, 2, 1, 0 };
+  static constexpr uint8_t kModes_020[4] = { 0, 0, 2, 0 };
+  static constexpr uint8_t kModes_021[4] = { 0, 1, 2, 0 };
+  static constexpr uint8_t kModes_022[4] = { 0, 2, 2, 0 };
+  static constexpr uint8_t kModes_200[4] = { 0, 0, 0, 2 };
+  static constexpr uint8_t kModes_201[4] = { 0, 1, 0, 2 };
+  static constexpr uint8_t kModes_202[4] = { 0, 2, 0, 2 };
+  static constexpr uint8_t kModes_210[4] = { 0, 0, 1, 2 };
+  static constexpr uint8_t kModes_211[4] = { 0, 1, 1, 2 };
+  static constexpr uint8_t kModes_212[4] = { 0, 2, 1, 2 };
+  static constexpr uint8_t kModes_220[4] = { 0, 0, 2, 2 };
+  static constexpr uint8_t kModes_221[4] = { 0, 1, 2, 2 };
+  static constexpr uint8_t kModes_222[4] = { 0, 2, 2, 2 };
+
 
   struct IntcodeComputer {
     std::vector<int64_t> mem;
     std::vector<int64_t> out;
     int64_t ip = 0, rel = 0, size = 0;
+    const uint8_t* modes = nullptr;
+
 
     bool load(const char* filename) {
       FILE* f = fopen(filename, "r");
@@ -33,15 +53,17 @@ namespace vh {
     }
 
     int64_t peek(int64_t i) const {
-      int64_t mode = (mem[ip] / kModeDiv[i]) % 10;
-      int64_t addr = ((mode == 1) ? (ip + i) : mem[ip + i]) + ((mode == 2) ? rel : 0);
+      int64_t addr;
+      switch (modes[i]) {
+        case 0: addr = mem[ip + i]; break;
+        case 1: addr = ip + i; break;
+        case 2: addr = mem[ip + i] + rel; break;
+      }
       return (addr >= size) ? 0 : mem[addr];
     }
 
     int64_t& poke(int64_t i) {
-      int64_t mode = (mem[ip] / kModeDiv[i]) % 10;
-//      int64_t addr = ((mode == 1) ? (ip + i) : mem[ip + i]) + ((mode == 2) ? rel : 0);
-      int64_t addr = mem[ip + i] + ((mode == 2) ? rel : 0);
+      int64_t addr = mem[ip + i] + (modes[i] >> 1) * rel;
       if (addr >= size) {
         size = addr + INTCODE_MEM_SIZE;
         mem.resize(static_cast<size_t>(size), 0ll);
@@ -53,8 +75,30 @@ namespace vh {
     int run(std::deque<int64_t>& input) {
       out.clear();
       while (1) {
-        int64_t op = mem[ip] % 100;
-        switch (op) {
+        int op = static_cast<int>(mem[ip]);
+        switch (op / 100) {
+          case   0: modes = kModes_000; break;
+          case   1: modes = kModes_001; break;
+          case   2: modes = kModes_002; break;
+          case  10: modes = kModes_010; break;
+          case  11: modes = kModes_011; break;
+          case  12: modes = kModes_012; break;
+          case  20: modes = kModes_020; break;
+          case  21: modes = kModes_021; break;
+          case  22: modes = kModes_022; break;
+          case 200: modes = kModes_200; break;
+          case 201: modes = kModes_201; break;
+          case 202: modes = kModes_202; break;
+          case 210: modes = kModes_210; break;
+          case 211: modes = kModes_211; break;
+          case 212: modes = kModes_212; break;
+          case 220: modes = kModes_220; break;
+          case 221: modes = kModes_221; break;
+          case 222: modes = kModes_222; break;
+          default: modes = nullptr; break;
+        }
+
+        switch (op % 100) {
           case 1: poke(3) = peek(1) + peek(2); ip += 4; break; // add
           case 2: poke(3) = peek(1) * peek(2); ip += 4; break; // mul
           case 3: if (input.empty()) return 1; poke(1) = input.front(); input.pop_front(); ip += 2; break; // read
