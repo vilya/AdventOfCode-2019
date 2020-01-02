0156497809da9dd07da8fce32abef57dd0783f9c
Vilya Harvey <vilya.harvey@gmail.com>
2020-01-02 23:14:31 +0000

    mmap a large memory region instead of using a std::vector
    We now simply mmap a large amount of memory up front, without committing it, instead of using a std::vector for storage. This means the OS's memory manager automatically handles adding new pages as necessary and the pages will be zeroed when we get them. This, in turn, means we don't need any out-of-bounds checks and we never need to resize the memory. Overall it ends up being a really big performance improvement (see the README.md in intcode/benchmarks for the updated benchmark results).

--- intcode/tiny-intcode.h
+++ intcode/tiny-intcode.h
@@ -3,68 +3,61 @@
 #include <queue>
 #include <vector>
 
-#ifndef INTCODE_MEM_SIZE
-#define INTCODE_MEM_SIZE (128 * 1024)
-#endif
+#include <sys/mman.h>
 
 namespace vh {
   
+  static constexpr int64_t kMemSize = 256 * 1024 * 1024;
+
   struct IntcodeComputer {
-    std::vector<int64_t> mem;
+    int64_t* mem;
     std::vector<int64_t> out;
-    int64_t ip = 0, rel = 0, size = 0;
+    int64_t ip = 0, rel = 0;
     const uint8_t* modes = nullptr;
 
+    IntcodeComputer() {
+      mem = reinterpret_cast<int64_t*>(mmap(nullptr, kMemSize * sizeof(int64_t), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE | MAP_NORESERVE, -1, 0));
+      out.reserve(16);
+    }
+
+    ~IntcodeComputer() {
+      munmap(mem, kMemSize * sizeof(int64_t));
+    }
 
     bool load(const char* filename) {
       FILE* f = fopen(filename, "r");
       if (f == nullptr)
         return false;
-      mem.reserve(INTCODE_MEM_SIZE);
-      int64_t tmp;
+      int64_t tmp, i = 0;
       while (fscanf(f, "%lld,", &tmp) == 1)
-        mem.push_back(tmp);
+        mem[i++] = tmp;
       fclose(f);
-      if (mem.size() < INTCODE_MEM_SIZE)
-        mem.resize(INTCODE_MEM_SIZE, 0ll);
-      size = static_cast<int64_t>(mem.size());
-      out.reserve(16);
       return true;
     }
 
     int64_t peek0(int64_t i) const {
       int64_t addr = mem[ip + i];
-      return (addr >= size) ? 0 : mem[addr];
+      return mem[addr];
     }
 
     int64_t peek1(int64_t i) const {
       int64_t addr = ip + i;
-      return (addr >= size) ? 0 : mem[addr];
+      return mem[addr];
     }
 
     int64_t peek2(int64_t i) const {
       int64_t addr = mem[ip + i] + rel;
-      return (addr >= size) ? 0 : mem[addr];
+      return mem[addr];
     }
 
-    int64_t& poke0(int64_t i) {
+    void poke0(int64_t i, int64_t val) {
       int64_t addr = mem[ip + i];
-      if (addr >= size) {
-        size = addr + INTCODE_MEM_SIZE;
-        mem.resize(static_cast<size_t>(size), 0ll);
-        fprintf(stderr, "resize mem to %lld\n", size);
-      }
-      return mem[addr];
+      mem[addr] = val;
     }
 
-    int64_t& poke2(int64_t i) {
+    void poke2(int64_t i, int64_t val) {
       int64_t addr = mem[ip + i] + rel;
-      if (addr >= size) {
-        size = addr + INTCODE_MEM_SIZE;
-        mem.resize(static_cast<size_t>(size), 0ll);
-        fprintf(stderr, "resize mem to %lld\n", size);
-      }
-      return mem[addr];
+      mem[addr] = val;
     }
 
     // return value: 0 = halted, 1 = need input
@@ -74,48 +67,48 @@ namespace vh {
         int op = static_cast<int>(mem[ip]);
         switch (op) {
           // variants of add
-          case     1: poke0(3) = peek0(1) + peek0(2); ip += 4; break;
-          case   101: poke0(3) = peek1(1) + peek0(2); ip += 4; break;
-          case   201: poke0(3) = peek2(1) + peek0(2); ip += 4; break;
-          case  1001: poke0(3) = peek0(1) + peek1(2); ip += 4; break;
-          case  1101: poke0(3) = peek1(1) + peek1(2); ip += 4; break;
-          case  1201: poke0(3) = peek2(1) + peek1(2); ip += 4; break;
-          case  2001: poke0(3) = peek0(1) + peek2(2); ip += 4; break;
-          case  2101: poke0(3) = peek1(1) + peek2(2); ip += 4; break;
-          case  2201: poke0(3) = peek2(1) + peek2(2); ip += 4; break;
-          case 20001: poke2(3) = peek0(1) + peek0(2); ip += 4; break;
-          case 20101: poke2(3) = peek1(1) + peek0(2); ip += 4; break;
-          case 20201: poke2(3) = peek2(1) + peek0(2); ip += 4; break;
-          case 21001: poke2(3) = peek0(1) + peek1(2); ip += 4; break;
-          case 21101: poke2(3) = peek1(1) + peek1(2); ip += 4; break;
-          case 21201: poke2(3) = peek2(1) + peek1(2); ip += 4; break;
-          case 22001: poke2(3) = peek0(1) + peek2(2); ip += 4; break;
-          case 22101: poke2(3) = peek1(1) + peek2(2); ip += 4; break;
-          case 22201: poke2(3) = peek2(1) + peek2(2); ip += 4; break;
+          case     1: poke0(3, peek0(1) + peek0(2)); ip += 4; break;
+          case   101: poke0(3, peek1(1) + peek0(2)); ip += 4; break;
+          case   201: poke0(3, peek2(1) + peek0(2)); ip += 4; break;
+          case  1001: poke0(3, peek0(1) + peek1(2)); ip += 4; break;
+          case  1101: poke0(3, peek1(1) + peek1(2)); ip += 4; break;
+          case  1201: poke0(3, peek2(1) + peek1(2)); ip += 4; break;
+          case  2001: poke0(3, peek0(1) + peek2(2)); ip += 4; break;
+          case  2101: poke0(3, peek1(1) + peek2(2)); ip += 4; break;
+          case  2201: poke0(3, peek2(1) + peek2(2)); ip += 4; break;
+          case 20001: poke2(3, peek0(1) + peek0(2)); ip += 4; break;
+          case 20101: poke2(3, peek1(1) + peek0(2)); ip += 4; break;
+          case 20201: poke2(3, peek2(1) + peek0(2)); ip += 4; break;
+          case 21001: poke2(3, peek0(1) + peek1(2)); ip += 4; break;
+          case 21101: poke2(3, peek1(1) + peek1(2)); ip += 4; break;
+          case 21201: poke2(3, peek2(1) + peek1(2)); ip += 4; break;
+          case 22001: poke2(3, peek0(1) + peek2(2)); ip += 4; break;
+          case 22101: poke2(3, peek1(1) + peek2(2)); ip += 4; break;
+          case 22201: poke2(3, peek2(1) + peek2(2)); ip += 4; break;
 
           // variants of mul
-          case     2: poke0(3) = peek0(1) * peek0(2); ip += 4; break;
-          case   102: poke0(3) = peek1(1) * peek0(2); ip += 4; break;
-          case   202: poke0(3) = peek2(1) * peek0(2); ip += 4; break;
-          case  1002: poke0(3) = peek0(1) * peek1(2); ip += 4; break;
-          case  1102: poke0(3) = peek1(1) * peek1(2); ip += 4; break;
-          case  1202: poke0(3) = peek2(1) * peek1(2); ip += 4; break;
-          case  2002: poke0(3) = peek0(1) * peek2(2); ip += 4; break;
-          case  2102: poke0(3) = peek1(1) * peek2(2); ip += 4; break;
-          case  2202: poke0(3) = peek2(1) * peek2(2); ip += 4; break;
-          case 20002: poke2(3) = peek0(1) * peek0(2); ip += 4; break;
-          case 20102: poke2(3) = peek1(1) * peek0(2); ip += 4; break;
-          case 20202: poke2(3) = peek2(1) * peek0(2); ip += 4; break;
-          case 21002: poke2(3) = peek0(1) * peek1(2); ip += 4; break;
-          case 21102: poke2(3) = peek1(1) * peek1(2); ip += 4; break;
-          case 21202: poke2(3) = peek2(1) * peek1(2); ip += 4; break;
-          case 22002: poke2(3) = peek0(1) * peek2(2); ip += 4; break;
-          case 22102: poke2(3) = peek1(1) * peek2(2); ip += 4; break;
-          case 22202: poke2(3) = peek2(1) * peek2(2); ip += 4; break;
+          case     2: poke0(3, peek0(1) * peek0(2)); ip += 4; break;
+          case   102: poke0(3, peek1(1) * peek0(2)); ip += 4; break;
+          case   202: poke0(3, peek2(1) * peek0(2)); ip += 4; break;
+          case  1002: poke0(3, peek0(1) * peek1(2)); ip += 4; break;
+          case  1102: poke0(3, peek1(1) * peek1(2)); ip += 4; break;
+          case  1202: poke0(3, peek2(1) * peek1(2)); ip += 4; break;
+          case  2002: poke0(3, peek0(1) * peek2(2)); ip += 4; break;
+          case  2102: poke0(3, peek1(1) * peek2(2)); ip += 4; break;
+          case  2202: poke0(3, peek2(1) * peek2(2)); ip += 4; break;
+          case 20002: poke2(3, peek0(1) * peek0(2)); ip += 4; break;
+          case 20102: poke2(3, peek1(1) * peek0(2)); ip += 4; break;
+          case 20202: poke2(3, peek2(1) * peek0(2)); ip += 4; break;
+          case 21002: poke2(3, peek0(1) * peek1(2)); ip += 4; break;
+          case 21102: poke2(3, peek1(1) * peek1(2)); ip += 4; break;
+          case 21202: poke2(3, peek2(1) * peek1(2)); ip += 4; break;
+          case 22002: poke2(3, peek0(1) * peek2(2)); ip += 4; break;
+          case 22102: poke2(3, peek1(1) * peek2(2)); ip += 4; break;
+          case 22202: poke2(3, peek2(1) * peek2(2)); ip += 4; break;
 
           // variants of read
-          case   3: if (input.empty()) return 1; poke0(1) = input.front(); input.pop_front(); ip += 2; break;
-          case 203: if (input.empty()) return 1; poke2(1) = input.front(); input.pop_front(); ip += 2; break;
+          case   3: if (input.empty()) return 1; poke0(1, input.front()); input.pop_front(); ip += 2; break;
+          case 203: if (input.empty()) return 1; poke2(1, input.front()); input.pop_front(); ip += 2; break;
 
           // variants of write
           case   4: out.push_back(peek0(1)); ip += 2; break;
@@ -145,44 +138,44 @@ namespace vh {
           case 2206: ip = (peek2(1) == 0) ? peek2(2) : (ip + 3); break;
 
           // variants of less-than
-          case     7: poke0(3) = (peek0(1) < peek0(2)) ? 1 : 0; ip += 4; break;
-          case   107: poke0(3) = (peek1(1) < peek0(2)) ? 1 : 0; ip += 4; break;
-          case   207: poke0(3) = (peek2(1) < peek0(2)) ? 1 : 0; ip += 4; break;
-          case  1007: poke0(3) = (peek0(1) < peek1(2)) ? 1 : 0; ip += 4; break;
-          case  1107: poke0(3) = (peek1(1) < peek1(2)) ? 1 : 0; ip += 4; break;
-          case  1207: poke0(3) = (peek2(1) < peek1(2)) ? 1 : 0; ip += 4; break;
-          case  2007: poke0(3) = (peek0(1) < peek2(2)) ? 1 : 0; ip += 4; break;
-          case  2107: poke0(3) = (peek1(1) < peek2(2)) ? 1 : 0; ip += 4; break;
-          case  2207: poke0(3) = (peek2(1) < peek2(2)) ? 1 : 0; ip += 4; break;
-          case 20007: poke2(3) = (peek0(1) < peek0(2)) ? 1 : 0; ip += 4; break;
-          case 20107: poke2(3) = (peek1(1) < peek0(2)) ? 1 : 0; ip += 4; break;
-          case 20207: poke2(3) = (peek2(1) < peek0(2)) ? 1 : 0; ip += 4; break;
-          case 21007: poke2(3) = (peek0(1) < peek1(2)) ? 1 : 0; ip += 4; break;
-          case 21107: poke2(3) = (peek1(1) < peek1(2)) ? 1 : 0; ip += 4; break;
-          case 21207: poke2(3) = (peek2(1) < peek1(2)) ? 1 : 0; ip += 4; break;
-          case 22007: poke2(3) = (peek0(1) < peek2(2)) ? 1 : 0; ip += 4; break;
-          case 22107: poke2(3) = (peek1(1) < peek2(2)) ? 1 : 0; ip += 4; break;
-          case 22207: poke2(3) = (peek2(1) < peek2(2)) ? 1 : 0; ip += 4; break;
+          case     7: poke0(3, (peek0(1) < peek0(2)) ? 1 : 0); ip += 4; break;
+          case   107: poke0(3, (peek1(1) < peek0(2)) ? 1 : 0); ip += 4; break;
+          case   207: poke0(3, (peek2(1) < peek0(2)) ? 1 : 0); ip += 4; break;
+          case  1007: poke0(3, (peek0(1) < peek1(2)) ? 1 : 0); ip += 4; break;
+          case  1107: poke0(3, (peek1(1) < peek1(2)) ? 1 : 0); ip += 4; break;
+          case  1207: poke0(3, (peek2(1) < peek1(2)) ? 1 : 0); ip += 4; break;
+          case  2007: poke0(3, (peek0(1) < peek2(2)) ? 1 : 0); ip += 4; break;
+          case  2107: poke0(3, (peek1(1) < peek2(2)) ? 1 : 0); ip += 4; break;
+          case  2207: poke0(3, (peek2(1) < peek2(2)) ? 1 : 0); ip += 4; break;
+          case 20007: poke2(3, (peek0(1) < peek0(2)) ? 1 : 0); ip += 4; break;
+          case 20107: poke2(3, (peek1(1) < peek0(2)) ? 1 : 0); ip += 4; break;
+          case 20207: poke2(3, (peek2(1) < peek0(2)) ? 1 : 0); ip += 4; break;
+          case 21007: poke2(3, (peek0(1) < peek1(2)) ? 1 : 0); ip += 4; break;
+          case 21107: poke2(3, (peek1(1) < peek1(2)) ? 1 : 0); ip += 4; break;
+          case 21207: poke2(3, (peek2(1) < peek1(2)) ? 1 : 0); ip += 4; break;
+          case 22007: poke2(3, (peek0(1) < peek2(2)) ? 1 : 0); ip += 4; break;
+          case 22107: poke2(3, (peek1(1) < peek2(2)) ? 1 : 0); ip += 4; break;
+          case 22207: poke2(3, (peek2(1) < peek2(2)) ? 1 : 0); ip += 4; break;
 
           // variants of equals
-          case     8: poke0(3) = (peek0(1) == peek0(2)) ? 1 : 0; ip += 4; break;
-          case   108: poke0(3) = (peek1(1) == peek0(2)) ? 1 : 0; ip += 4; break;
-          case   208: poke0(3) = (peek2(1) == peek0(2)) ? 1 : 0; ip += 4; break;
-          case  1008: poke0(3) = (peek0(1) == peek1(2)) ? 1 : 0; ip += 4; break;
-          case  1108: poke0(3) = (peek1(1) == peek1(2)) ? 1 : 0; ip += 4; break;
-          case  1208: poke0(3) = (peek2(1) == peek1(2)) ? 1 : 0; ip += 4; break;
-          case  2008: poke0(3) = (peek0(1) == peek2(2)) ? 1 : 0; ip += 4; break;
-          case  2108: poke0(3) = (peek1(1) == peek2(2)) ? 1 : 0; ip += 4; break;
-          case  2208: poke0(3) = (peek2(1) == peek2(2)) ? 1 : 0; ip += 4; break;
-          case 20008: poke2(3) = (peek0(1) == peek0(2)) ? 1 : 0; ip += 4; break;
-          case 20108: poke2(3) = (peek1(1) == peek0(2)) ? 1 : 0; ip += 4; break;
-          case 20208: poke2(3) = (peek2(1) == peek0(2)) ? 1 : 0; ip += 4; break;
-          case 21008: poke2(3) = (peek0(1) == peek1(2)) ? 1 : 0; ip += 4; break;
-          case 21108: poke2(3) = (peek1(1) == peek1(2)) ? 1 : 0; ip += 4; break;
-          case 21208: poke2(3) = (peek2(1) == peek1(2)) ? 1 : 0; ip += 4; break;
-          case 22008: poke2(3) = (peek0(1) == peek2(2)) ? 1 : 0; ip += 4; break;
-          case 22108: poke2(3) = (peek1(1) == peek2(2)) ? 1 : 0; ip += 4; break;
-          case 22208: poke2(3) = (peek2(1) == peek2(2)) ? 1 : 0; ip += 4; break;
+          case     8: poke0(3, (peek0(1) == peek0(2)) ? 1 : 0); ip += 4; break;
+          case   108: poke0(3, (peek1(1) == peek0(2)) ? 1 : 0); ip += 4; break;
+          case   208: poke0(3, (peek2(1) == peek0(2)) ? 1 : 0); ip += 4; break;
+          case  1008: poke0(3, (peek0(1) == peek1(2)) ? 1 : 0); ip += 4; break;
+          case  1108: poke0(3, (peek1(1) == peek1(2)) ? 1 : 0); ip += 4; break;
+          case  1208: poke0(3, (peek2(1) == peek1(2)) ? 1 : 0); ip += 4; break;
+          case  2008: poke0(3, (peek0(1) == peek2(2)) ? 1 : 0); ip += 4; break;
+          case  2108: poke0(3, (peek1(1) == peek2(2)) ? 1 : 0); ip += 4; break;
+          case  2208: poke0(3, (peek2(1) == peek2(2)) ? 1 : 0); ip += 4; break;
+          case 20008: poke2(3, (peek0(1) == peek0(2)) ? 1 : 0); ip += 4; break;
+          case 20108: poke2(3, (peek1(1) == peek0(2)) ? 1 : 0); ip += 4; break;
+          case 20208: poke2(3, (peek2(1) == peek0(2)) ? 1 : 0); ip += 4; break;
+          case 21008: poke2(3, (peek0(1) == peek1(2)) ? 1 : 0); ip += 4; break;
+          case 21108: poke2(3, (peek1(1) == peek1(2)) ? 1 : 0); ip += 4; break;
+          case 21208: poke2(3, (peek2(1) == peek1(2)) ? 1 : 0); ip += 4; break;
+          case 22008: poke2(3, (peek0(1) == peek2(2)) ? 1 : 0); ip += 4; break;
+          case 22108: poke2(3, (peek1(1) == peek2(2)) ? 1 : 0); ip += 4; break;
+          case 22208: poke2(3, (peek2(1) == peek2(2)) ? 1 : 0); ip += 4; break;
 
           // variants of set-relative-base
           case   9: rel += peek0(1); ip += 2; break;
